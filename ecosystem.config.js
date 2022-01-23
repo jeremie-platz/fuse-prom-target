392f59977d63ae48626459b9358e3f6abb809a18
Jaime Pericás <jpericas22@gmail.com>
2022-01-24 00:10:10 +0100

    pm2 name update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "fuse-prom-target",
+      name: "fuse-prom-target-arbitrum",
       script: "npm",
       args: "run start-target",
       cron_restart: "0 * * * *" // Restart every hour.
     },
     {
-      name: "fuse-prom-proxy",
+      name: "fuse-prom-proxy-arbitrum",
       script: "npm",
       args: "run start-proxy"
     }
